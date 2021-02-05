1c8a78cb06c5e23f2ff12361bf18b9f31ac0df75
Lucas Bovolini <lbovolini94@gmail.com>
2021-02-05 18:51:22 -0300

    Adicionado testes do modelo de usuario

--- src/tests/unit/UserModelTest.php
+++ src/tests/unit/UserModelTest.php
@@ -11,8 +11,9 @@ use App\Models\UserModel;
  */
 class UserModelTest extends CIUnitTestCase {
 
-    public $mockDatabaseConnection;
+    private $mockDatabaseConnection;
     private $userModel;
+    private $userTable = 'users';
 
     public function __construct() {
         parent::__construct();
@@ -23,6 +24,8 @@ class UserModelTest extends CIUnitTestCase {
         $this->userModel = new UserModel($this->mockDatabaseConnection);
     }
 
+    // findAndDelete method test cases
+
     /**
      * @test
      */
@@ -30,6 +33,7 @@ class UserModelTest extends CIUnitTestCase {
 
         $userId = 1;
         $expectedNumberOfRowInFindResult = 1;
+        $expectedTransactionStatus = true;
 
         // Query Result Mock
         $resultMock = $this->getMockBuilder(BaseResult::class)
@@ -48,11 +52,212 @@ class UserModelTest extends CIUnitTestCase {
 
         $this->mockDatabaseConnection->expects($this->once())
             ->method('transStatus')
-            ->will($this->returnValue(true));
+            ->will($this->returnValue($expectedTransactionStatus));
 
         // Should test ONLY this method
         $isUserFound = $this->userModel->findAndDelete($userId);
 
         $this->assertTrue($isUserFound);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotFindAndDeleteUserUsingId() {
+
+        $userId = 2;
+        $expectedNumberOfRowInFindResult = 0;
+        $expectedTransactionStatus = true;
+
+        // Query Result Mock
+        $resultMock = $this->getMockBuilder(BaseResult::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // Query Result Mock behaviors
+        $resultMock->expects($this->once())
+            ->method('getNumRows')
+            ->will($this->returnValue($expectedNumberOfRowInFindResult));
+
+        // Database Connection Mock behaviors
+        $this->mockDatabaseConnection->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue($resultMock));
+
+        $this->mockDatabaseConnection->expects($this->once())
+            ->method('transStatus')
+            ->will($this->returnValue($expectedTransactionStatus));
+
+        // Should test ONLY this method
+        $isUserFound = $this->userModel->findAndDelete($userId);
+
+        $this->assertFalse($isUserFound);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowRuntimeExceptionWhenFindAndDeleteTransactionFails() {
+
+        $userId = 1;
+        $expectedNumberOfRowInFindResult = 0;
+        $expectedTransactionStatus = false;
+
+        // Query Result Mock
+        $resultMock = $this->getMockBuilder(BaseResult::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // Query Result Mock behaviors
+        $resultMock->expects($this->once())
+            ->method('getNumRows')
+            ->will($this->returnValue($expectedNumberOfRowInFindResult));
+
+        // Database Connection Mock behaviors
+        $this->mockDatabaseConnection->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue($resultMock));
+
+        $this->mockDatabaseConnection->expects($this->once())
+            ->method('transStatus')
+            ->will($this->returnValue($expectedTransactionStatus));
+
+        // Assertions
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Error executing delete query in {$this->userTable} table");
+
+        // Should test ONLY this method
+        $isUserFound = $this->userModel->findAndDelete($userId);
+    }
+
+    // findAndUpdate method test cases
+
+    /**
+     * @test
+     */
+    public function shouldFindAndUpdateUsingValidId() {
+
+        $expectedNumberOfRowInFindResult = 1;
+        $expectedTransactionStatus = true;
+
+        $userData = new StdClass();
+        $userData->id = 1;
+        $userData->first_name = 'Lucas';
+        $userData->last_name = 'Bovolini';
+        $userData->email = 'lbovolini94@gmail.com';
+        $userData->username = 'lbovolini';
+        $userData->password = 'pass11word';
+        $userData->birthday = '1994/07/18';
+
+        // Query Result Mock
+        $resultMock = $this->getMockBuilder(BaseResult::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // Query Result Mock behaviors
+        $resultMock->expects($this->once())
+            ->method('getNumRows')
+            ->will($this->returnValue($expectedNumberOfRowInFindResult));
+
+        // Database Connection Mock behaviors
+        $this->mockDatabaseConnection->expects($this->exactly(2))
+            ->method('query')
+            ->will($this->returnValue($resultMock));
+
+        $this->mockDatabaseConnection->expects($this->once())
+            ->method('transStatus')
+            ->will($this->returnValue($expectedTransactionStatus));
+
+        // Should test ONLY this method
+        $isUserFound = $this->userModel->findAndUpdate($userData->id, $userData);
+
+        $this->assertTrue($isUserFound);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotFindAndUpdateUserUsingId() {
+
+        $userId = 2;
+        $expectedNumberOfRowInFindResult = 0;
+        $expectedTransactionStatus = true;
+
+        $userData = new StdClass();
+        $userData->id = 1;
+        $userData->first_name = 'Lucas';
+        $userData->last_name = 'Bovolini';
+        $userData->email = 'lbovolini94@gmail.com';
+        $userData->username = 'lbovolini';
+        $userData->password = 'pass11word';
+        $userData->birthday = '1994/07/18';
+
+        // Query Result Mock
+        $resultMock = $this->getMockBuilder(BaseResult::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // Query Result Mock behaviors
+        $resultMock->expects($this->once())
+            ->method('getNumRows')
+            ->will($this->returnValue($expectedNumberOfRowInFindResult));
+
+        // Database Connection Mock behaviors
+        $this->mockDatabaseConnection->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue($resultMock));
+
+        $this->mockDatabaseConnection->expects($this->once())
+            ->method('transStatus')
+            ->will($this->returnValue($expectedTransactionStatus));
+
+        // Should test ONLY this method
+        $isUserFound = $this->userModel->findAndUpdate($userId, $userData);
+
+        $this->assertFalse($isUserFound);
+    }
+
+        /**
+     * @test
+     */
+    public function shouldThrowRuntimeExceptionWhenFindAndUpdateTransactionFails() {
+
+        $expectedNumberOfRowInFindResult = 0;
+        $expectedTransactionStatus = false;
+
+        $userData = new StdClass();
+        $userData->id = 1;
+        $userData->first_name = 'Lucas';
+        $userData->last_name = 'Bovolini';
+        $userData->email = 'lbovolini94@gmail.com';
+        $userData->username = 'lbovolini';
+        $userData->password = 'pass11word';
+        $userData->birthday = '1994/07/18';
+
+        // Query Result Mock
+        $resultMock = $this->getMockBuilder(BaseResult::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // Query Result Mock behaviors
+        $resultMock->expects($this->once())
+            ->method('getNumRows')
+            ->will($this->returnValue($expectedNumberOfRowInFindResult));
+
+        // Database Connection Mock behaviors
+        $this->mockDatabaseConnection->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue($resultMock));
+
+        $this->mockDatabaseConnection->expects($this->once())
+            ->method('transStatus')
+            ->will($this->returnValue($expectedTransactionStatus));
+
+        // Assertions
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Error executing update query in {$this->userTable} table");
+
+        // Should test ONLY this method
+        $isUserFound = $this->userModel->findAndUpdate($userData->id, $userData);
     }
 }
