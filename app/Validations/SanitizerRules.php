515ea9a49895447e9dfbe49579b0c851ee40da4b
Lucas Bovolini <lbovolini94@gmail.com>
2021-02-10 17:01:51 -0300

    Refatoracao

--- app/Validations/SanitizerRules.php
+++ app/Validations/SanitizerRules.php
@@ -6,19 +6,19 @@ namespace App\Validations;
 
 class SanitizerRules {
 
-    public function is_sanitized_number($number): bool {
+    public function is_sanitized_number(int $number): bool {
         $sanitizedNumber = filter_var($number, FILTER_SANITIZE_NUMBER_INT);
 
         return $number == $sanitizedNumber;
     }
 
-    public function is_sanitized_string($string): bool {
+    public function is_sanitized_string(string $string): bool {
         $sanitizedString = filter_var($string, FILTER_SANITIZE_STRING);
 
         return strcmp($string, $sanitizedString) === 0;
     }
 
-    public function is_sanitized_email($email): bool {
+    public function is_sanitized_email(string $email): bool {
         $sanitizedEmail= filter_var($email, FILTER_SANITIZE_EMAIL);
 
         return strcmp($email, $sanitizedEmail) === 0;
